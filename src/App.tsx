007c2787c4cbe52235253f210ab9415497f0bb73
arikriff <arikriff@gmail.com>
2023-03-31 18:19:10 +0300

    init react typescript project display map view with feature layer of cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import esriConfig from "@arcgis/core/config";
+
+import WebMap from "@arcgis/core/WebMap";
+import Map from '@arcgis/core/Map';
+import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+// import * as projection from "@arcgis/core/geometry/projection";
+
+esriConfig.assetsPath = "./assets"; 
+esriConfig.apiKey = "AAPK8a6843e0d83f47bdaeae9a5259ec04a7GULvXyb7NdSzp0oEVM2EQTkExPas9lPWETDa35QtWufktGRYfiz_TXAHdbUFfQiw";
+
 function App() {
+  const [state, setState] = React.useState(1);
+
+  const map = React.useRef(new Map({
+    basemap: "arcgis-topographic" // Basemap layer service
+  }));
+  const mapView = React.useRef<MapView>();
+  const citiesLayer = React.useRef(new FeatureLayer({
+    url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Cities/FeatureServer/0'
+  }));
+  
+  React.useEffect(() => {
+    map.current.add(citiesLayer.current);
+    mapView.current = new MapView({
+      map: map.current,
+      center: [35.025128, 31.373781], // Longitude, latitude
+      zoom: 7, // Zoom level
+      container: "mapViewDiv"
+    });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      שלום!
+      <div id="mapViewDiv" style={{ width: 1000, height: 500 }} />
     </div>
   );
 }
